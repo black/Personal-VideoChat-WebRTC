ed857059831e32fde61f3ac8831ab85ed3a7893f
Λ B H I N Λ V <ixd.abhi@gmail.com>
2021-06-07 02:09:02 +0530

    fixed layout

--- Client/index.js
+++ Client/index.js
@@ -10,30 +10,75 @@ let localStream = null
 let remoteStream = null
 let localVideo = $('#localVideo')
 let remoteVideo = $('#remoteVideo')
-let camStart = $('#startCam')
-let createCall = $('#createCall')
+let webcamButton = $('#startCam')
+let callButton = $('#createCall')
+let answerButton = $('#answerCall')
 
-camStart.on('click', async () => {
+webcamButton.on('click', async () => {
     localStream = await navigator.mediaDevices.getUserMedia({
         video: true,
         audio: true
     })
-
     remoteStream = new MediaStream()
+
     localStream.getTracks().forEach((track) => {
         pc.addTrack(track, localStream)
-        console.log("localStream")
     })
 
     pc.ontrack = event => {
         console.log("peer track")
         event.stream[0].getTracks().forEach(track => {
             remoteStream.addTrack(track)
-            console.log("remoteStream")
         });
     };
 
     localVideo.srcObject = localStream
     remoteVideo.srcObject = remoteStream
+
+    callButton.disabled = false;
+    answerButton.disabled = false;
+    webcamButton.disabled = true;
+});
+
+callButton.on('click', async () => {
+
+    // Get candidates for caller, save to db
+    // Create offer
+    const offerDescription = await pc.createOffer();
+    await pc.setLocalDescription(offerDescription);
+
+    const offer = {
+        sdp: offerDescription.sdp,
+        type: offerDescription.type,
+    };
+
+    await callDoc.set({
+        offer
+    });
+
+    // Listen for remote answer
+    // callDoc.onSnapshot((snapshot) => {
+    //     const data = snapshot.data();
+    //     if (!pc.currentRemoteDescription && data ? .answer) {
+    //         const answerDescription = new RTCSessionDescription(data.answer);
+    //         pc.setRemoteDescription(answerDescription);
+    //     }
+    // });
+
+    // When answered, add candidate to peer connection
+    answerCandidates.onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+            if (change.type === 'added') {
+                const candidate = new RTCIceCandidate(change.doc.data());
+                pc.addIceCandidate(candidate);
+            }
+        });
+    });
+
+    hangupButton.disabled = false;
+
+});
+
+answerButton.on('click', async () => {
 
 });
